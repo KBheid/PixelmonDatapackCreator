679af98226fab74a05ba21b542ef1a0ca438e10f
Kayle <kbheid@live.com>
2023-10-20 08:32:24 -0600

    In progress

---
+++ PixelmonPokemonCreator/Assets/Scripts/UI/Editors/MovementEditors/MovementEditor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovementEditor : MonoBehaviour
+{
+    [SerializeField] List<Selectable> selectablesToToggle_Rideable;
+    [SerializeField] List<Selectable> selectablesToToggle_Fly;
+    [SerializeField] List<Selectable> selectablesToToggle_Surf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
